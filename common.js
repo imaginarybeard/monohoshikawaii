0e360d627e30410eaaa9d65ec286e85dbd13dd7c
インカ <inca@MacBookPro13-inca.local>
2019-04-28 06:22:43 +0900

    降臨ごとのプリセットを選択可能に

--- common.js
+++ common.js
@@ -11,12 +11,45 @@ var aiueo = [
 "わ","ん"
 ]
 
+var enemy_params = [
+  ["プラチナーガ", "左+3右+2", "3:2"],
+  ["ハイアシンス", "+4", "4"],
+  ["ゴシェナイト", "+2", "2"],
+  ["トルマリン", "+1", "1"],
+  ["クバルフ", "奇数ターン+12偶数ターン+24", "12:24"],
+  ["ルーレットゥポーン", "-9", "-9"],
+  ["マガツカ", "+9", "9"],
+  ["エヴァ１３号機", "+8", "8"],
+]
+
 var diff_numbers = []
 var column_root_chars = []
 
 var root_middle_row_element = $('<div class="middle_row"><button class="btn btn-danger diff_button delete_row"><div class="delete_mark">x</div> <div class="value">1</div></button></div>')
 var user_middle_row_element = $('<div class="middle_row"><div></div></div>')
 var user_column = $('<div class="user_column"><div class="first_row"><button class="btn btn-danger round_button delete_button delete_column"><div class="round_mark">x</div></button></div><div class="second_row"><div></div></div></div>')
+
+var change_enemy = (enemy_diffs)=>{
+  delete_diff_numbers()
+  if(enemy_diffs != ""){
+    var diffs_list = enemy_diffs.split(":")
+    for(var i in diffs_list){
+      add_diff_number(Number(diffs_list[i]))
+    }
+  }
+}
+
+var generate_select_options = ()=>{
+  var generate_select_option = (option_params)=>{
+    var option = $('<option/>')
+    option.attr({ 'value': option_params[2] }).text(option_params[0] + "： " + option_params[1])
+    $("#enemy_select").append(option)
+  }
+
+  for(var i in enemy_params){
+    generate_select_option(enemy_params[i])
+  }
+}
 
 var check_diff_number = ()=>{
   var number = $("#diff_number_input").val()
@@ -151,15 +184,21 @@ var delete_cookie = ()=>{
   document.cookie = "columnrootchars" + '='
 }
 
-var delete_value = ()=>{
-  var diff_numbers = []
-  var column_root_chars = []
-
+var delete_diff_numbers = ()=>{
+  diff_numbers = []
   var diff_number_eles = $("#root_column").find(".middle_row")
-  var user_columns = $("#user_columns").find(".user_column")
-
   diff_number_eles.remove()
+}
+
+var delete_user_columns = ()=>{
+  column_root_chars = []
+  var user_columns = $("#user_columns").find(".user_column")
   user_columns.remove()
+}
+
+var delete_value = ()=>{
+  delete_diff_numbers()
+  delete_user_columns()
 }
 
 var set_cookie_value = ()=>{
@@ -173,6 +212,7 @@ var set_cookie_value = ()=>{
 
 $(document).ready(function(){
   load_cookie()
+  generate_select_options()
 
   $('#add_row').on('click', function(){
     push_add_diff_number_button()
@@ -204,6 +244,10 @@ $(document).ready(function(){
 
   $("#addition_char_select").change(function() {
     push_add_column_button()
+  });
+
+  $("#enemy_select").change(function() {
+    change_enemy($(this).val())
   });
 
   $('#user_columns').on('click', ".delete_column", function(){
